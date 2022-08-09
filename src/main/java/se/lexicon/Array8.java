754cba91712eeb322a00818bfd0005f3a1b423f2
Rubotnic <101702711+Rubotnic@users.noreply.github.com>
2022-08-09 16:46:37 +0200

    Update Array8.java
    Cleaning code!

--- src/main/java/se/lexicon/Array8.java
+++ src/main/java/se/lexicon/Array8.java
@@ -9,44 +9,42 @@ public class Array8 {
     public void exerEight() {
 
         {
-            int array[] = {20,20,40,20,30,40,50,60,50};
-            System.out.println();
+            int numbers[] = {20,20,40,20,30,40,50,60,50};
             System.out.print("Array: ");
-            for (int i = 0; i<array.length; i++)
-            {
-                System.out.print(array[i]+"\t");
+
+            for (int i=0; i<numbers.length; i++) {
+                System.out.print(numbers[i] + " ");
             }
-            for (int i=0; i<array.length; i++)
-            {
-                for (int j=i; j<array.length; j++)
-                {
-                    if (array[i] > array[j]) {
-                        int temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
+            for (int i = 0; i < numbers.length; i++) {
+
+                for (int j = i; j < numbers.length; j++) {
+
+                    if (numbers[i] > numbers[j]) {
+                        int temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
                     }
                 }
             }
 
-            for (int i=0; i<array.length; i++)
-            {
+            for (int i=0; i< numbers.length; i++) {
             }
-            System.out.print ("\nArray without duplicate values: ");
+            System.out.print('\n' + "Array without duplicate values: ");
 
             int b=0;
-            array[b] = array[0];
+            numbers[b] = numbers[0];
 
-            for (int i=0; i<array.length; i++)
+            for (int i=0; i<numbers.length; i++)
             {
-                if (array[b] != array[i])
+                if (numbers[b] != numbers[i])
                 {
                     b++;
-                    array[b] = array[i];
+                    numbers[b] = numbers[i];
                 }
             }
             for (int i=0; i<=b; i++)
             {
-                System.out.print(array[i]+"\t");
+                System.out.print(numbers[i]+ " ");
             }
         }
     }
